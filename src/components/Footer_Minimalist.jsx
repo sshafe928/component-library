7629c190ff764bdaedb714be036da8d72e0d0a1d
Sabrina Shafer <141285407+sshafe928@users.noreply.github.com>
2024-11-20 15:15:42 -0700

    beginning new component

---
+++ src/components/Footer_Minimalist.jsx
@@ -0,0 +1,34 @@
+const ProfileCard = ({
+    logo = //uploaded cloudinary logo ,
+    services = {Shipping:'link', returns_exchanges: 'link', contact: 'link' },
+    socials ={Instagram: 'link', Github:'link', Facebook: 'link'},
+    info ={privacy: 'privacy', about: 'link', terms_of_service: 'terms of service link' }
+}) => {
+    return (
+        <div className="footer">
+            <div className="logo">{logo}</div>
+            <div className="about">
+                <ul>
+                    <li className="Shipping">{services.Shipping}</li>
+                    <li className="returns_exchanges">{services.returns_exchanges}</li>
+                    <li className="conact">{services.contact}</li>
+                </ul>
+            </div>
+            <div className="socials">
+                <ul>
+                    <li className="insta">{socials.Instagram}</li>
+                    <li className="github">{socials.Github}</li>
+                    <li className="facebook">{socials.Facebook}</li>
+                </ul>
+            </div>
+            <div className="services">
+                <ul>
+                    <li className="privacy">{info.privacy}</li>
+                    <li className="about">{info.about}</li>
+                    <li className="terms_of_service">{info.terms_of_service}</li>
+                </ul>
+            </div>
+
+        </div>
+    )
+}
