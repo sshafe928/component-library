24ea7adca49892e49ffe46dac50eb5a9e005f089
Sabrina Shafer <sshafe928@west-mec.org>
2024-11-21 19:38:51 -0700

    button updates

---
+++ src/pages/main.jsx
@@ -0,0 +1,21 @@
+// import '../pages/css/ProfilePage.css';
+
+function MainNav() {
+
+    
+    return (
+        <>
+    <a href="/profile"><buttom className="btn">Profile Card</buttom></a>
+    <a href="/footer" ><buttom className="btn">Minimal Footer</buttom></a>
+    <a href=""><buttom className="btn"></buttom></a>
+    <a href=""><buttom className="btn"></buttom></a>
+    <a href=""><buttom className="btn"></buttom></a>
+    <a href=""><buttom className="btn"></buttom></a>
+    <a href=""><buttom className="btn"></buttom></a>
+    <a href="" ><buttom className="btn"></buttom></a>
+
+        </>
+    );
+}
+
+export default MainNav;
